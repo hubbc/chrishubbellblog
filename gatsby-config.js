21e319c883c555dbd3c66104979c174bf31194dc
hubbellcc <hubbellc@msoe.edu>
2021-12-11 12:25:34 -0600

    changed to my name

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Chris' Blog`,
     author: {
-      name: `Kyle Mathews`,
+      name: `Chris Hubbell`,
       summary: `who lives and works in San Francisco building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
